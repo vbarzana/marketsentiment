47d9d38da81f941b1de8503629a3b56dd37f5bd4
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2018-06-07 16:06:28 +0300

    Modifying the ports...

--- config/env/staging.js
+++ config/env/staging.js
@@ -1,5 +1,5 @@
 /**
- * Development environment settings
+ * Staging environment settings
  *
  * This file can include shared settings for a development team,
  * such as API keys or remote database passwords.  If you're using
