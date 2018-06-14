a7d3fdc8eaf6ab027acca0fdf29f167e64155d62
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2018-06-14 03:44:54 +0300

    [fix] Providing StockTwits API configuration.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,38 +8,10 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-const request = require('request');
 const _ = require('lodash');
 const glob = require('glob');
 
 module.exports.bootstrap = function (cb) {
-  // @todo: have a view to open the prompt from stocktwits to get the secret code
-  // request(sails.config.stocktwits.oauthUrl, function (error, response, body) {
-  //   error && console.log(error);
-  //   console.log(body);
-  //   sails.renderView(body);
-  // });
-  if (!sails.config.stocktwits.accessToken) {
-    request.post({
-      url: sails.config.stocktwits.getTokenUrl,
-      form: {
-        client_id: sails.config.stocktwits.clientId,
-        client_secret: sails.config.stocktwits.clientSecret,
-        code: sails.config.stocktwits.secretCode,
-        grant_type: 'authorization_code',
-        redirect_uri: sails.config.stocktwits.redirectUrl
-      }
-    }, function (error, response, body) {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      console.log(body);
-      sails.config.stocktwits.accessToken = _.get(body, 'access_token');
-      console.log(sails.config.stocktwits.accessToken);
-    });
-  }
-
   sails.after('hook:orm:loaded', function () {
     initializeFixtures();
   });
--- config/stocktwits.js
+++ config/stocktwits.js
@@ -3,22 +3,8 @@
  * (sails.config.stocktwits)
  *
  */
-  // @todo: move all this configuration out of here to a secure location
-const redirectUrl = 'http://market-sentiment.herokuapp.com/connect/stocktwits/callback';
-const apiUrl = 'https://api.stocktwits.com/api/2';
-const clientId = '3dfaa132db4373c2';
-const clientSecret = '40cb179f04ecff94add54e0248309c8f5b42c99d';
-const code = 'a10fcb4632057a76bfc6130151162ee247d473e5';
-const scope = 'read,watch_lists,publish_messages,publish_watch_lists,direct_messages,follow_users,follow_stocks';
-const accessToken = 'd3e3e94ac0b9b33aa2f5a9de87f4e286dd5d7672';
-
 module.exports.stocktwits = {
-  apiUrl: apiUrl,
-  oauthUrl: `${apiUrl}/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUrl}&scope=${scope}`,
-  getTokenUrl: `${apiUrl}/oauth/token`,
-  clientId: clientId,
-  secretCode: code,
-  accessToken: accessToken,
-  clientSecret: clientSecret,
-  redirectUrl: redirectUrl
+  clientId: process.env.STOCKTWITS_CLIENT_ID,
+  apiUrl: process.env.STOCKTWITS_API_URL,
+  accessToken: process.env.STOCKTWITS_ACCESS_TOKEN
 };
