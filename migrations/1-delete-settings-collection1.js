d1132b5998660e4e111360f58816c679ad5ba380
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-09-03 01:16:11 +0000

    deleting unnecessary migration

--- migrations/1-delete-settings-collection1.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  id: __filename.replace('.js', ''),
-  up: function (done) {
-    this.db.collection('settings').drop();
-    // use this.db for MongoDB communication, and this.log() for logging
-    done();
-  }
-};
