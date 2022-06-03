4f6c5d30774524702ef66cd036a6565ddd25025c
vbarzana <viticoinf@gmail.com>
2022-06-03 15:37:31 +0300

    session fixed

--- config/session.js
+++ config/session.js
@@ -14,89 +14,51 @@
 
 module.exports.session = {
 
-  /***************************************************************************
-   *                                                                          *
-   * Session secret is automatically generated when your new app is created   *
-   * Replace at your own risk in production-- you will invalidate the cookies *
-   * of your users, forcing them to log in again.                             *
-   *                                                                          *
-   ***************************************************************************/
+  /**
+   * @cfg secret
+   *
+   * Session secret is automatically generated when your new app is created
+   * Replace at your own risk in production-- you will invalidate the cookies
+   * of your users, forcing them to log in again.
+   */
   secret: '0f6e5b16306e7bb5cb40c5b61239cdfa',
 
+  /**
+   * @cfg {Object} cookie
+   * @cfg {Number} [cookie.maxAge="24 * 60 * 60 * 1000"]
+   *
+   * Set the session cookie expire time The maxAge is set by milliseconds,
+   * the example below is for 24 hours
+   */
+  cookie: {
+    maxAge: 24 * 365 * 60 * 60 * 1000
+  },
 
-  /***************************************************************************
-   *                                                                          *
-   * Set the session cookie expire time The maxAge is set by milliseconds,    *
-   * the example below is for 24 hours                                        *
-   *                                                                          *
-   ***************************************************************************/
-
-  // cookie: {
-  //   maxAge: 24 * 60 * 60 * 1000
-  // },
-
-  /***************************************************************************
-   *                                                                          *
-   * Uncomment the following lines to set up a Redis session store that can   *
-   * be shared across multiple Sails.js servers.                              *
-   *                                                                          *
-   * Requires connect-redis (https://www.npmjs.com/package/connect-redis)     *
-   *                                                                          *
-   ***************************************************************************/
-
-  // adapter: 'redis',
-
-  /***************************************************************************
-   *                                                                          *
-   * The following values are optional, if no options are set a redis         *
-   * instance running on localhost is expected. Read more about options at:   *
-   *                                                                          *
-   * https://github.com/visionmedia/connect-redis                             *
-   *                                                                          *
-   ***************************************************************************/
-
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>,
-  // prefix: 'sess:',
-
-
-  /***************************************************************************
-   *                                                                          *
-   * Uncomment the following lines to set up a MongoDB session store that can *
-   * be shared across multiple Sails.js servers.                              *
-   *                                                                          *
-   * Requires connect-mongo (https://www.npmjs.com/package/connect-mongo)     *
-   * Use version 0.8.2 with Node version <= 0.12                              *
-   * Use the latest version with Node >= 4.0                                  *
-   *                                                                          *
-   ***************************************************************************/
-  collection: 'sessions',
+  /**
+   * @cfg adapter Adapter for storing sessions
+   */
   adapter: 'connect-mongo',
-  uri: process.env.MONGODB_URI,
-  // adapter: 'mongo',
-  // url: 'mongodb://user:password@localhost:27017/dbname', // user, password and port optional
 
-  /***************************************************************************
-   *                                                                          *
-   * Optional Values:                                                         *
-   *                                                                          *
-   * See https://github.com/kcbanner/connect-mongo for more                   *
-   * information about connect-mongo options.                                 *
-   *                                                                          *
-   * See http://bit.ly/mongooptions for more information about options        *
-   * available in `mongoOptions`                                              *
-   *                                                                          *
-   ***************************************************************************/
+  /**
+   * @cfg host Mongodb hostname
+   */
+  host: 'localhost',
 
-  // collection: 'sessions',
-  // stringify: true,
-  // mongoOptions: {
-  //   server: {
-  //     ssl: true
-  //   }
-  // }
+  /**
+   * @cfg port Mongodb port
+   */
+  port: 27017,
 
+  /**
+   * @cfg db Mongodb database
+   */
+  db: 'market-sentiment',
+
+  /**
+   * @cfg collection Mongodb collection using to store sessions
+   */
+  collection: 'sessions',
+
+  saveUninitialized: false,
+  resave: false
 };
