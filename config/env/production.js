6c3f1d1b29ddd4ffe7eb61a5c81164d433b63464
vbarzana <viticoinf@gmail.com>
2022-06-03 14:48:36 +0300

    fix: Providing a Procfile for Heroku.

--- config/env/production.js
+++ config/env/production.js
@@ -11,34 +11,53 @@
  */
 
 module.exports = {
-
-  orm: {
-    _hookTimeout: 50000
-  },
-
   log: {
     level: 'debug'
   },
 
-  /***************************************************************************
-   * Set the default database connection for models in the production        *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
   models: {
     migrate: 'safe'
   },
 
-  /***************************************************************************
-   * Set the port in the production environment to 80                        *
-   ***************************************************************************/
-
-  port: process.env.PORT || 80,
+  session: {
+    adapter: 'connect-mongo',
+    uri: process.env.MONGODB_URI
+  },
 
   connections: {
     mongodb: {
       adapter: 'sails-mongo',
       url: process.env.MONGODB_URI
     }
-  }
+  },
 
+  stocktwits: {
+    clientId: process.env.STOCKTWITS_CLIENT_ID,
+    apiUrl: process.env.STOCKTWITS_API_URL,
+    accessToken: process.env.STOCKTWITS_ACCESS_TOKEN
+  },
+
+  twitter: {
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  },
+
+  port: process.env.PORT || 1337,
+
+  cors: {
+    allRoutes: true,
+    origin: '*',
+    credentials: false
+  },
+
+  discord: {
+    clientId: process.env.DISCORD_CLIENT_ID,
+    token: process.env.DISCORD_TOKEN,
+    nasdaqNewsChannelId: process.env.DISCORD_NASDAQ_NEWS_CHANNEL_ID,
+    twitterChannelId: process.env.DISCORD_TWITTER_CHANNEL_ID,
+    premarketNewsChannelId: process.env.DISCORD_PREMARKET_NEWS_CHANNEL_ID,
+    oauthUrl: process.env.DISCORD_OAUTH_URL
+  }
 };
